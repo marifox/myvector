21d80c76752b917ffe7b0a85bb7dd39a0821ec5a
Maria <maria.yazhuk@gmail.com>
2019-04-12 18:49:41 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,8 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
 class myvector {
     int sz;
     double* elem;
 public:
-    myvector(int s);
-    int sizze() const { return sz; }
+    myvector(int s) :
+        sz{s},
+        elem{new double[s]}
+        {
+            for(int i = 0; i < s; i++) {
+                elem[i] = 0;
+            }
+        }
+    ~myvector() { delete[] elem; }
 
+    int sizze() const { return sz; }
 };
+int main()
+{
+    cout << sizeof( int) << endl;
+    return 0;
+}
+
